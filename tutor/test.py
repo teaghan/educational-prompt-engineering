d4a579b2e474526dd4626c143322cb66a839a188
Teaghan O'Briain <obriaintb@gmail.com>
2024-08-31 14:20:04 +0000

    streamlit

---
+++ tutor/test.py
@@ -0,0 +1,39 @@
+from openai import OpenAI
+import streamlit as st
+
+def load_api_key(key_file):
+    with open(key_file) as f:
+        key = f.read().strip("\n")
+    return key
+
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["OPENAI_API_KEY"] = load_api_key('../key.txt')
+os.environ["LANGCHAIN_API_KEY"] = load_api_key(key_file='../langchain_key.txt')
+os.environ['USER_AGENT'] = 'myagent'
+
+with st.sidebar:
+    openai_api_key = load_api_key('../key.txt')
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
+st.title("💬 Chatbot")
+st.caption("🚀 A Streamlit chatbot powered by OpenAI")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    client = OpenAI(api_key=openai_api_key)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message.content
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
