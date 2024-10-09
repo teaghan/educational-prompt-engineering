7b9f01aa5ad0ecf88bdd48f8a4a523f0ddabf365
Teaghan O'Briain <obriaintb@gmail.com>
2024-10-09 20:53:42 +0000

    ai tutors

--- ai_tutors/main.py
+++ ai_tutors/main.py
@@ -2,38 +2,40 @@ import streamlit as st
 import pandas as pd
 #from st_files_connection import FilesConnection
 
-ai_tutors_data_fn = 'ai-tutors/myfile.csv'
+ai_tutors_data_fn = 'ai-tutors/tutor_info.csv'
 
-st.set_page_config(page_title="AI Science Tutor", page_icon="https://raw.githubusercontent.com/teaghan/educational-prompt-engineering/main/images/science_tutor_favicon_small.png", layout="wide")
+st.set_page_config(page_title="AI Tutors", page_icon="https://raw.githubusercontent.com/teaghan/educational-prompt-engineering/main/images/science_tutor_favicon_small.png", layout="wide")
 
-#st.markdown("<h1 style='text-align: center; color: grey;'>AI Science Tutor</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: grey;'>Build an AI Tutor</h1>", unsafe_allow_html=True)
 
-
-'''
 # Create connection object and retrieve file contents.
 conn = st.connection('s3', type=FilesConnection, ttl=0)
 df = conn.read(ai_tutors_data_fn, input_format="csv", ttl=0)
 
+st.text(dir(conn))
+
 # Print existing records.
 st.write("Existing records:")
 for row in df.itertuples():
-    st.write(f"{row.Owner} has a :{row.Pet}:")
+    st.write(f"Name: {row.Name}")
+    st.markdown(f"{row.Instructions}")
+    st.markdown(f"{row.Guidelines}")
 
 # Add a new row via user input
 st.write("Add a new record:")
-new_owner = st.text_input("Owner name")
-new_pet = st.text_input("Pet type")
+new_name = st.text_input("Name")
+new_instr = st.text_input("Instructions")
+new_guide = st.text_input("Guidelines")
 
-if st.button("Add new row"):
-    if new_owner and new_pet:
+if st.button("Add new tutor"):
+    if new_name and new_instr and new_guide:
         # Create a new row as a DataFrame
-        new_row = pd.DataFrame({"Owner": [new_owner], "Pet": [new_pet]})
+        new_row = pd.DataFrame({"Name": [new_name], "Instructions": [new_instr], "Guidelines": [new_guide]})
         # Concatenate the new row to the DataFrame
         df = pd.concat([df, new_row], ignore_index=True)
         with conn.open(ai_tutors_data_fn, "wt") as f:
             df.to_csv(f, index=False)
         st.success("New row added and saved to the cloud!")
     else:
-        st.error("Please provide both Owner and Pet information.")
+        st.error("Please provide all of the info.")
     st.rerun()
-'''
