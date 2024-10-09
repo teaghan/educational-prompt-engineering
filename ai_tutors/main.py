b59e097b2855e8b99c41bbc665ffd7f3acb92654
Teaghan O'Briain <obriaintb@gmail.com>
2024-10-09 20:08:12 +0000

    ai tutors

--- ai_tutors/main.py
+++ ai_tutors/main.py
@@ -2,9 +2,8 @@ import streamlit as st
 from st_files_connection import FilesConnection
 
 # Create connection object and retrieve file contents.
-# Specify input format is a csv and to cache the result for 600 seconds.
 conn = st.connection('s3', type=FilesConnection)
-df = conn.read("myfile.csv", input_format="csv", ttl=0)
+df = conn.read("ai-tutors/myfile.csv", input_format="csv", ttl=0)
 
 # Print results.
 for row in df.itertuples():
